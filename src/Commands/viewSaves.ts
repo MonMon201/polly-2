fa3ff422370e6ff98a9c07e8bbbf703b035da884
MonMon201 <danyavasilinenko213@gmail.com>
2021-06-28 15:18:47 +0300

    chore: remote ext in viewSaves command

--- src/Commands/viewSaves.ts
+++ src/Commands/viewSaves.ts
@@ -8,10 +8,12 @@ import { showMessage } from '../Utils/showMessage';
 const viewSaves = async (message: Message) => {
     const savesRoute = `${__dirname}/../../saves`;
     if (!existsSync(savesRoute)) return;
-    const saves = readdirSync(savesRoute).filter((file) => {
-        const ext = path.extname(file).toLowerCase();
-        return ext === '.json';
-    });
+    const saves = readdirSync(savesRoute)
+        .filter((filename) => {
+            const ext = path.extname(filename).toLowerCase();
+            return ext === '.json';
+        })
+        .map((filename) => filename.split('.')[0]);
     console.log(`parsed saves: ${saves}`);
     await Promise.all(
         saves.map(async (save) => await showMessage(message, save)),
