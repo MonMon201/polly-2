cd7db66f90527b77e1a98cbee7666e55355a8d0a
MonMon201 <danyavasilinenko213@gmail.com>
2021-06-28 16:05:21 +0300

    chore: update whitelist

--- src/Utils/whiteList.ts
+++ src/Utils/whiteList.ts
@@ -5,6 +5,5 @@ import { getConfig } from "../Env/environment";
 export const checkWhiteList = (message: Message): boolean => {
     const { prod } = getConfig();
     const whiteList = prod ? config.prodWhiteList : config.devWhiteList;
-    console.log(whiteList)
     return !!whiteList.find((el) => el === message.guild?.id || '');
 }
