be91b0fc4f4acd0f609f45ba9957deef03464ddb
MonMon201 <danyavasilinenko213@gmail.com>
2021-06-28 13:41:11 +0300

    chore: test launch

--- dist/Commands/viewSaves.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=viewSaves.js.map
--- dist/Utils/runPoll.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.runPoll = void 0;
-const runPoll = async (message, options) => options.map(async (option) => {
-    const msg = await message.channel.send(option);
-    await msg.react('➕');
-});
-exports.runPoll = runPoll;
-//# sourceMappingURL=runPoll.js.map
