7e05f18f68a1a261c84a8f6881b2cc065d8874f3
MonMon201 <danyavasilinenko213@gmail.com>
2021-06-28 20:52:26 +0300

    chore: delete commands

--- src/Utils/setDiscordCommands.ts
+++ src/Utils/setDiscordCommands.ts
@@ -1,4 +1,5 @@
 import { ApplicationCommand } from 'discord-slash-commands-client';
+import { deleteApiCommands } from './deleteApiCommands';
 import { getApiCommands } from './getApiCommands';
 import { getCommands } from './getCommands';
 import { updateCommands } from './updateCommands';
@@ -11,9 +12,11 @@ export const setDiscordBotCommands = async (
     const url = `https://discord.com/api/v8/applications/${clientId}/commands`;
     const commands = await getCommands();
     const apiCommandsResponse = await getApiCommands(token, url);
-    if (commands.length === apiCommandsResponse.data.length) {
-      return apiCommandsResponse.data;
-    }
-    const updatedApiCommands = await updateCommands(commands, url, token, []);
-    return updatedApiCommands.map((updatedApiCommand) => updatedApiCommand.data);
+    const deletedCommandsResponse = await deleteApiCommands(apiCommandsResponse.data.map((command) => command.id), [], token, url)
+    return deletedCommandsResponse.map((deletedCommand) => deletedCommand.data);
+    // if (commands.length === apiCommandsResponse.data.length) {
+    //   return apiCommandsResponse.data;
+    // }
+    // const updatedApiCommands = await updateCommands(commands, url, token, []);
+    // return updatedApiCommands.map((updatedApiCommand) => updatedApiCommand.data);
 };
