f0c7f0d1c55e2767c146eba14d53db0180beb2b2
MonMon201 <danyavasilinenko213@gmail.com>
2021-06-28 16:02:39 +0300

    chore: test launch

--- src/Utils/whiteList.ts
+++ src/Utils/whiteList.ts
@@ -5,5 +5,6 @@ import { getConfig } from "../Env/environment";
 export const checkWhiteList = (message: Message): boolean => {
     const { prod } = getConfig();
     const whiteList = prod ? config.prodWhiteList : config.devWhiteList;
+    console.log(whiteList)
     return !!whiteList.find((el) => el === message.guild?.id || '');
 }
