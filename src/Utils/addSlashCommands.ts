bfefbd688729d2ac5debc04448199d0fedcf3cbc
MonMon201 <danyavasilinenko213@gmail.com>
2022-01-10 23:28:15 +0200

    chore: remove token

--- src/Utils/addSlashCommands.ts
+++ src/Utils/addSlashCommands.ts
@@ -1,16 +1,8 @@
-import { getConfig } from "../Env/environment";
 import { getCommands } from "./getCommands";
-
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
 import { Polly } from "../Client/Client";
 
 // Place your client and guild ids here
-const clientId = '9x79ys8rHTmCvFeo0LfgUXe8KW45TfO2';
-const token = 'NzIzMTUwOTMzMjc3ODAyNTA3.XutcyQ.Beid4H73pi28Xu1sdThXs-6Af-8';
 const guildId = '722393393296179211';
-
-const rest = new REST({ version: '9' }).setToken(token);
 
 export const addSlashCommands = async (polly: Polly) => {
     const guild = polly.guilds.cache.get(guildId);
